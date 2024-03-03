c74c8557bf8d9f55eb3a9d90dc5724eeaf8dbd10
Luan Almeida Tobias <94020355+Lu4nAlmeida@users.noreply.github.com>
2024-03-03 04:05:19 -0300

    Update Collect_Images.py

--- Collect_Images.py
+++ Collect_Images.py
@@ -8,7 +8,7 @@ if not os.path.exists(DATA_DIR):
 
 # Parameters
 number_of_classes = 3
-dataset_size = 100
+dataset_size = 200
 
 # Loop for each hand pose chosen in "number_of_classes"
 cap = cv2.VideoCapture(0)
