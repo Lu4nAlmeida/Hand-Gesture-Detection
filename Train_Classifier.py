1230645662418db978451fa0c75d15f9b53d6800
Luan Almeida Tobias <94020355+Lu4nAlmeida@users.noreply.github.com>
2024-03-03 04:06:38 -0300

    Update Train_Classifier.py

--- Train_Classifier.py
+++ Train_Classifier.py
@@ -13,22 +13,22 @@ labels = np.asarray(data_dict['labels'])
 
 # Split data into two sets one for training and one for testing
 # Test set only contains 20% of the data, the other 80% is used for training
-x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
+data_train, data_test, labels_train, labels_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
 # Define classifier
 model = RandomForestClassifier()
 
 # Train classifier
-model.fit(x_train, y_train)
+model.fit(data_train, labels_train)
 
 # Test classifier
-y_predict = model.predict(x_test)
+label_predict = model.predict(data_test)
 
 # Compute accuracy
-score = accuracy_score(y_predict, y_test)
+score = accuracy_score(label_predict, labels_test)
 print('{}% of samples were classified correctly'.format(score * 100))
 
-# Save our trained classifier as model.p
+# Save our trained classifier as "model.p"
 f = open('model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
